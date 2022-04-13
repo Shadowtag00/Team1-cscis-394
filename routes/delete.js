107a04726f6be239608828dc10c3095bf4cd4b47
mallen54 <mjallen247@gmail.com>
2022-04-12 21:12:12 -0500

    adding delete

--- routes/delete.js
+++ routes/delete.js
@@ -5,18 +5,18 @@ var router = express.Router();
 router.get('/', (req, res)=> {
     pool.query('SELECT * FROM comments', (err, comments_results) => {
         console.log(err, comments_results)
-        res.render('/delete', {
+        res.render('delete', {
             comments: comments_results.rows
         })
 
     })
 });
-
+/*
 router.get('/:comment_id', (req, res)=> {
     pool.query(`DELETE FROM comments WHERE comment_id = '${req.params.delete_id}'`, (err, results) => {
         console.log(err, results)
         res.redirect('/')
     })
 });
-
+*/
 module.exports = router;
