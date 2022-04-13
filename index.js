23ac95bf3c95437f19299a0a1905be19cec817b7
Shadowtag00 <nealmandaliya@gmail.com>
2022-04-13 17:03:20 -0500

    console log

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ app.get('/comments/:id/form', (req, res) => {
     const id = req.params["id"]
     
     pool.query(`SELECT is_flagged FROM comments WHERE id=${id}`, (err, result) => {
-        console.log(err, results)
+        console.log(err, result)
         if (result.rows.length == 0) {
             res.status(404)
             return
