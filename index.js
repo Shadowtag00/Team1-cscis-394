ee0b6e18938873de9864067c75074a58b9e414f6
Aneri <anerimadhu@gmail.com>
2022-04-13 15:36:37 -0500

    added methodOverride

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
 const { urlencoded } = require('express');
 const port = process.env.PORT || 3002
 //basic authentication
