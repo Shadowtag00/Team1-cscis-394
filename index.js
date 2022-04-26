67f0b897891043b11244881115f104c449851a50
mallen54 <mjallen247@gmail.com>
2022-04-25 19:32:24 -0500

    commented out app.locals.moment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-app.locals.moment = require('moment');						//added - calling moment() will give current date and time
+//app.locals.moment = require('moment');						//added - calling moment() will give current date and time
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const { urlencoded } = require('express');
