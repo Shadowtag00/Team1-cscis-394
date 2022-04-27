a8413a24c760334d79c6dec002cf21741150a896
Justin Soell <48267898+jls99@users.noreply.github.com>
2022-04-27 16:22:53 -0500

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -90,7 +90,7 @@ var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function staticHeader() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
