3ab1ba64ab99b693373b19fa15695fc84809cdba
nadolny1 <nadolny.christopher1@gmail.com>
2022-04-27 02:10:59 -0500

    Testing home page

--- routes/home.js
+++ routes/home.js
@@ -2,16 +2,16 @@ var express = require("express");
 var router = express.Router();
 
 function adminonly(req,res,next){
-    //if (!req.session.isadmin) {
+    // if (!req.session.isadmin) {
     //    return res.redirect('/');
-    //}
+    // }
     next();
 }
 
 function checkLogin(req,res,next){ //verifies there's a user signed in
-    //if(!req.session.user_id){
+    // if(!req.session.user_id){
     //    return res.redirect('/');
-    //}
+    // }
     next();
 }
 
