e96f04fcdc3e8e3cb79b66ce1644742f35901703
nadolny1 <nadolny.christopher1@gmail.com>
2022-04-27 16:05:36 -0500

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -16,7 +16,7 @@ function checkLogin(req,res,next){ //verifies there's a user signed in
 }
 
 //CREATE (Add comment)
-router.post('/user_comment', checkLogin,(req,res) => {
+router.post('/', checkLogin,(req,res) => {
     
     //added this
     console.log(req.path)    
