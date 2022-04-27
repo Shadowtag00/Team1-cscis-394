3009608cf3a2ac0b6bfd6d12b5e886879802c3bd
Justin Soell <48267898+jls99@users.noreply.github.com>
2022-04-27 16:03:52 -0500

    static bar in admin

--- routes/admin.js
+++ routes/admin.js
@@ -80,7 +80,22 @@ router.get('/:comment_id/delete', adminonly, (req, res) => {
     })
 })
 
+// When the user scrolls the page, execute staticHeader
+window.onscroll = function() {staticHeader()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
 module.exports = router;
-
-
-
