53dd785e528abba87b4cafae5642cec7b4c97860
nadolny1 <nadolny.christopher1@gmail.com>
2022-04-27 19:37:07 -0500

    changed profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,39 +1,39 @@
-var express = require("express");
-var router = express.Router();
+// var express = require("express");
+// var router = express.Router();
 
-function checkLogin(req,res,next){ //verifies there's a user signed in
-    if(!req.session.user_id){
-       return res.redirect('/'); 
-    }
-    next();
-}
+// function checkLogin(req,res,next){ //verifies there's a user signed in
+//     if(!req.session.user_id){
+//        return res.redirect('/'); 
+//     }
+//     next();
+// }
 
-//READ (Display comments)
-router.get('/', checkLogin, (req, res) =>{
+// //READ (Display comments)
+// router.get('/', checkLogin, (req, res) =>{
 
-    console.log('Accept: ' + req.get('Accept'))
-    pool.query('SELECT VERSION()', (err, version_results) => {
-        //added this
-        if (err) {
-            return console.error('Error executing query', err.stack)
-        }       
+//     console.log('Accept: ' + req.get('Accept'))
+//     pool.query('SELECT VERSION()', (err, version_results) => {
+//         //added this
+//         if (err) {
+//             return console.error('Error executing query', err.stack)
+//         }       
 
-        console.log(err, version_results.rows)
+//         console.log(err, version_results.rows)
         
-        pool.query(`SELECT username, text FROM comments WHERE is_flagged='f' and username=${req.session.username}`, (err, comments_results) => {
-            console.log(err, comments_results)
+//         pool.query(`SELECT username, text FROM comments WHERE is_flagged='f' and username=${req.session.username}`, (err, comments_results) => {
+//             console.log(err, comments_results)
             
-            res.render('home', {
-                                    comments: comments_results.rows,
-                                    message: req.session.username
-                                })
-            console.log('Content-Type: ' + res.get('Content-Type'))
+//             res.render('home', {
+//                                     comments: comments_results.rows,
+//                                     message: req.session.username
+//                                 })
+//             console.log('Content-Type: ' + res.get('Content-Type'))
                             
-        })
-    })   
-})
+//         })
+//     })   
+// })
 
-module.exports = router;
+// module.exports = router;
 
 
 
