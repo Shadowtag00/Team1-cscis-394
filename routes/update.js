83614257c05427c83bbb816dd6b24a0170585d44
Aneri <anerimadhu@gmail.com>
2022-05-03 14:38:45 -0500

    working on update profile

---
+++ routes/update.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+function useronly(req,res,next){
+    if (!req.session.user_id)
+    {return res.redirect('/');}
+    next();
+    }
+
+function checkLogin(req,res,next){ //verifies there's a user signed in
+    if(!req.session.user_id){
+       return res.redirect('/'); 
+    }
+    next();
+}
