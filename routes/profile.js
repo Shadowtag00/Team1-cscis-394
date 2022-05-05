97a0d943d747893e607384e25d7ce47452be4eb6
Justin Soell <48267898+jls99@users.noreply.github.com>
2022-05-04 19:58:42 -0500

    profile edit

--- routes/profile.js
+++ routes/profile.js
@@ -15,7 +15,7 @@ function checkLogin(req,res,next){ //verifies there's a user signed in
 }
 
 //READ (Display comments)
-router.get('/', function(req, res, next) {
+router.get('/', useronly, (req, res) => {
     
     console.log('Accept: ' + req.get('Accept'))
     pool.query('SELECT VERSION()', (err, version_results) => {
