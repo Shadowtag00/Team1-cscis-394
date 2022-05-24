5e74befa202d8db4cce39ef379d4c860688d45e6
Shadowtag00 <nealmandaliya@gmail.com>
2022-05-24 18:39:53 -0500

    pagination on all pages

--- routes/reply.js
+++ routes/reply.js
@@ -66,7 +66,25 @@ router.get('/', checkLogin, (req, res) =>{
 
         //console.log(err, version_results.rows)
 
-        pool.query(`SELECT username, text FROM reply WHERE is_flagged='f'`, (err, reply_results) => {
+         //Pagination
+         const urlParams = new URLSearchParams(req.url)
+         console.log(urlParams)
+         if (urlParams.has('/?page')==false){
+             var pagenumber = 1
+         }
+         else{
+             var pagenumber = urlParams.get('/?page')
+         }
+         //console.log(pagenumber)
+         var offset = (pagenumber - 1) * 10
+         var page_count
+         //console.log(offset)
+         pool.query(`SELECT username, text, post_date FROM comments WHERE is_flagged='f'`, (err, pageCount)=>{
+             page_count = (pageCount.rowCount)/10
+             console.log(page_count)
+         })
+
+        pool.query(`SELECT username, text FROM reply WHERE is_flagged='f' LIMIT 10 OFFSET ${offset}`, (err, reply_results) => {
 	        //Already choose selected posts that weren't flagged
 		
             console.log(err, reply_results)
@@ -78,6 +96,7 @@ router.get('/', checkLogin, (req, res) =>{
             res.render('reply', {
                                     comments: "test",
                                     reply: reply_results.rows,
+                                    page_count: page_count-1,
                                     message: req.session.username
             })               
             console.log('Content-Type: ' + res.get('Content-Type'))
